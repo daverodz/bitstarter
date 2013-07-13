e950b2624b3c146e36f29c06705cd00051f569f6
David Rodriguez <davidrodriguez133@gmail.com>
2013-07-13 20:22:20 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,17 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
+var data = fs.readFileSync("./index.html","utf-8");
+var buffer = new Buffer(data);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(function(){ 
-  var buffer = new Buffer(fs.readFileSync('./index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-}),"utf-8");
-  console.log("Buffer is:"+buffer.toString('utf-8'));
-  return buffer.toString("utf-8");
-});
-
+  response.send(buffer.toString("utf-8");
 });
 
 var port = process.env.PORT || 5000;
